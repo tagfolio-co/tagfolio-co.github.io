ca69e71ea412d315d047c2c4f50bc4829763d5d8
Mike <mike@lemonshell.com>
2020-06-20 19:27:49 -0700

    Add first build

---
+++ precache-manifest.005b1e2286a9b5cb304ee7db0fe165a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f30b550677882029d06a76d47277a2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d98a5d50449859d23ce3",
+    "url": "/static/css/main.28457d26.chunk.css"
+  },
+  {
+    "revision": "76cf71a6124d3fb3fe30",
+    "url": "/static/js/2.53e3f013.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.53e3f013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d98a5d50449859d23ce3",
+    "url": "/static/js/main.cb5c1919.chunk.js"
+  },
+  {
+    "revision": "f092fbd489b395e34efc",
+    "url": "/static/js/runtime-main.3718e8e7.js"
+  }
+]);
